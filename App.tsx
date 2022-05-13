96c6171c5d65ad9339ffb9be2a5f9163b3f871e8
Brendan Thompson <clubysm2494@gmail.com>
2022-05-13 13:06:11 -0700

    YESSIR
    GOGOGOGOGO

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.top}>
+      <Text style={styles.header}>
+        <Image
+          source={require("./assets/circle-question-solid.png")}
+          style={styles.image}
+        />{" "}
+        Uni-Guesser!{" "}
+        <Image
+          source={require("./assets/ranking-star-solid.png")}
+          style={styles.image2}
+        />
+      </Text>
+      <View style={styles.container}>
+        <View style={styles.SquareShapeView} />
+        <View style={styles.SquareShapeView} />
+        <View style={styles.SquareShapeView} />
+        <View style={styles.SquareShapeView} />
+      </View>
+      <View style={styles.RectangleShapeView} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  top: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 200,
+    backgroundColor: "#0C2340",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 60,
+    alignItems: "center",
+    paddingLeft: "400px",
+    paddingRight: "400px",
+    borderBottomWidth: 3,
+    borderBottomColor: "#B2A268",
+    color: "#B2A268",
+  },
+  container: {
+    flexDirection: "row",
+  },
+  SquareShapeView: {
+    width: 300,
+    height: 300,
+    marginTop: "10%",
+    marginRight: "4%",
+    marginLeft: "-2%",
+    backgroundColor: "#0054A6",
+    borderWidth: 3,
+    borderColor: "#FFF200",
+  },
+  RectangleShapeView: {
+    marginTop: "15%",
+    width: 600,
+    height: 60,
+    backgroundColor: "#0054A6",
+    borderWidth: 3,
+    borderColor: "#FFF200",
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+  image2: {
+    width: 30,
+    height: 30,
   },
 });
