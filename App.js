29a1f645badefabe59ddf5a3f594754fabb268cc
Brendan Thompson <clubysm2494@gmail.com>
2022-05-15 17:15:04 -0700

    pls do not kill me

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, setState } from "react";
 import { Component } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
@@ -26,167 +26,153 @@ const Itest = [
   { src: "./assets/yup.jpg" },
   { src: "./assets/Horimiya-OP.webp" },
 ];
-const renderItem = ({ item }) => <Item uni={item.uni} />;
-class App extends React.Component {
-  // const [hints,setHints] = useState([])
 
-  images = () => {
+const renderItem = ({ item }) => <Item uni={item.uni} />;
+
+function App() {
+  const [hints, setHints] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [firstModal, setfirstModal] = useState(false);
+  const [secondModal, setsecondModal] = useState(false);
+  const [searchValue, setsearchValue] = useState();
+  const [data, setdata] = useState();
+
+  const toggleModal = () => {
+    setfirstModal(!firstModal);
+  };
+  const toggleModal2 = () => {
+    setsecondModal(!secondModal);
+  };
+
+  const images = () => {
     const random = [...Itest]
       .sort(() => Math.random() - 0.5)
       .map((hint) => ({ ...hint, id: Math.random() }));
 
     setHints(random);
+    setTurns(0);
   };
-
-  constructor() {
-    super();
-    this.state = {
-      firstModal: false,
-      secondModal: false,
-    };
-  }
-
-  render() {
-    return (
-      <View style={styles.top}>
-        <Text style={styles.header}>
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({ firstModal: true });
-            }}
-          >
-            <Image
-              source={require("./assets/circle-question-solid.png")}
-              style={styles.image}
-            />
-          </TouchableOpacity>{" "}
-          <Modal
-            transparent={true}
-            visible={this.state.firstModal}
-            style={{ width: 600, height: 600 }}
-          >
-            <View
-              style={{
-                backgroundColor: "#000000aa",
-                flex: 1,
-                alignItems: "center",
-              }}
-            >
-              <View
-                style={{
-                  marginTop: "5%",
-                  width: 600,
-                  height: 600,
-                  borderWidth: 3,
-                  borderColor: "#B2A268",
-                  backgroundColor: "#0054A6",
-                }}
-              >
-                <TouchableOpacity
-                  style={styles.Mimage}
-                  onPress={() => {
-                    this.setState({ firstModal: false });
-                  }}
-                >
-                  <Image
-                    source={require("./assets/circle-question-solid.png")}
-                    style={styles.Mimage}
-                  />
-                </TouchableOpacity>
-                <Text style={styles.test}>test test test</Text>
-              </View>
-            </View>
-          </Modal>
-          Uni-Guessr!{" "}
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({ secondModal: true });
-            }}
-          >
-            <Image
-              source={require("./assets/ranking-star-solid.png")}
-              style={styles.image}
-            />
-          </TouchableOpacity>
-          <Modal transparent={true} visible={this.state.secondModal}>
-            <View
-              style={{
-                backgroundColor: "#000000aa",
-                flex: 1,
-                alignItems: "center",
-              }}
-            >
-              <View
-                style={{
-                  marginTop: "5%",
-                  width: 600,
-                  height: 600,
-                  borderWidth: 3,
-                  borderColor: "#B2A268",
-                  backgroundColor: "#0054A6",
-                }}
-              >
-                <TouchableOpacity
-                  style={styles.Mimage}
-                  onPress={() => {
-                    this.setState({ secondModal: false });
-                  }}
-                >
-                  <Image
-                    source={require("./assets/ranking-star-solid.png")}
-                    style={styles.Mimage}
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
-          </Modal>
-        </Text>
-
-        <StatusBar style="auto" />
-        <View style={styles.container}>
-          <View style={styles.SquareShapeView}>
-            <Image
-              id="img"
-              style={styles.Squareimg}
-              source={require("./assets/ye.png")}
-            />
-          </View>
-          <View style={styles.SquareShapeView}>
-            <Image
-              style={styles.Squareimg}
-              source={require("./assets/yes.png")}
-            />
-          </View>
-          <View style={styles.SquareShapeView} />
-          <View style={styles.SquareShapeView} />
-        </View>
-
-        <View style={styles.searchList}>
-          <SearchBar
-            placeholder="Search Here..."
-            lightTheme
-            value={this.state.searchValue}
-            containerStyle={{
-              backgroundColor: "#0054A6",
-              borderWidth: 3,
-              borderBottomWidth: 3,
-              borderTopWidth: 3,
-              borderTopColor: "#FFF200",
-              borderBottomColor: "#FFF200",
-              borderColor: "#FFF200",
-            }}
-            onChangeText={(text) => this.searchFunction(text)}
-            autoCorrect={false}
+  console.log(hints, turns);
+  return (
+    <View style={styles.top}>
+      <Text style={styles.header}>
+        <TouchableOpacity onPress={toggleModal}>
+          <Image
+            source={require("./assets/circle-question-solid.png")}
+            style={styles.image}
           />
-          <FlatList
-            data={this.state.data}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
+        </TouchableOpacity>{" "}
+        <Modal
+          transparent={true}
+          visible={firstModal}
+          style={{ width: 600, height: 600 }}
+        >
+          <View
+            style={{
+              backgroundColor: "#000000aa",
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <View style={styles.modal}>
+              <TouchableOpacity style={styles.Mimage} onPress={toggleModal}>
+                <Image
+                  source={require("./assets/circle-question-solid.png")}
+                  style={styles.Mimage}
+                />
+              </TouchableOpacity>
+              <Text style={styles.test}>test test test</Text>
+            </View>
+          </View>
+        </Modal>
+        Uni-Guessr!{" "}
+        <TouchableOpacity onPress={toggleModal2}>
+          <Image
+            source={require("./assets/ranking-star-solid.png")}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        <Modal transparent={true} visible={secondModal}>
+          <View
+            style={{
+              backgroundColor: "#000000aa",
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <View style={styles.modal}>
+              <TouchableOpacity style={styles.Mimage} onPress={toggleModal2}>
+                <Image
+                  source={require("./assets/ranking-star-solid.png")}
+                  style={styles.Mimage}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </Text>
+
+      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <div className="grid">
+          {hints.map((hint) => (
+            <div className="card" key={hint.id}>
+              <div>
+                <img className="front" src={hint.src} />
+                <img
+                  className="back"
+                  src={require("./assets/yess.jpg")}
+                  alt="imgback"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <View style={styles.SquareShapeView}>
+          <Image
+            id="img"
+            style={styles.Squareimg}
+            source={require("./assets/ye.png")}
           />
         </View>
+        <View style={styles.SquareShapeView}>
+          <Image
+            style={styles.Squareimg}
+            source={require("./assets/yes.png")}
+          />
+        </View>
+        <View style={styles.SquareShapeView}></View>
+        <View style={styles.SquareShapeView} />
       </View>
-    );
-  }
+
+      <View style={styles.searchList}>
+        <SearchBar
+          placeholder="Search Here..."
+          lightTheme
+          value={searchValue}
+          containerStyle={{
+            backgroundColor: "#0054A6",
+            borderWidth: 3,
+            borderBottomWidth: 3,
+            borderTopWidth: 3,
+            borderTopColor: "#FFF200",
+            borderBottomColor: "#FFF200",
+            borderColor: "#FFF200",
+          }}
+          onChangeText={(text) => this.searchFunction(text)}
+          autoCorrect={false}
+        />
+        <button onClick={images}>Test</button>
+
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -222,6 +208,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#0054A6",
     borderWidth: 3,
     borderColor: "#FFF200",
+  },
+  modal: {
+    marginTop: "5%",
+    width: 600,
+    height: 600,
+    borderWidth: 3,
+    borderColor: "#B2A268",
+    backgroundColor: "#0054A6",
   },
   Squareimg: {
     width: 293,
